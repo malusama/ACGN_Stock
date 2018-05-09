585eddcd2d77f25d777b981d971c3ca70cd11af2
malusama <chensichengmalu@gmail.com>
2018-05-09 16:10:06 +0800

    fix bug

---
+++ app/static/baidu.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5ec26871c80d029485848dc4fa12d2f9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
